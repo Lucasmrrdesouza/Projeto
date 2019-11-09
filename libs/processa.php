d487e365c9585fbb8778c4986268de94ae7073c0
unknown <lucasmrrsouza@gmail.com>
2019-11-09 14:38:31 -0300

    ...

--- libs/processa.php
+++ libs/processa.php
@@ -10,14 +10,14 @@ $email = filter_input(INPUT_POST,'email', FILTER_SANITIZE_EMAIL);
 // echo "E-mail: $email <br>";
 
 
-$result_usuarios = "INSERT INTO usuarios (nome,email,criado) VALUES ('$nome', '$email', NOW())";
+$result_usuarios = "INSERT INTO usuarios (nome,email) VALUES ('$nome', '$email')";
 $resultado_usuario = mysqli_query($conn, $result_usuarios);
 
 if (mysqli_insert_id($conn)) {
 $_SESSION['msg'] = "<p style='color:green;'>Usuário cadastrado com sucesso</p>";
   header("Location: ../paginas/formulario.php");
 }else {
-  $_SESSION['msg'] = "<p style='color:green;'>Usuário não foi cadastrado com sucesso</p>";
+  $_SESSION['msg'] = "<p style='color:red;'>Usuário não foi cadastrado com sucesso</p>";
   header("Location: ../paginas/formulario.php");
 
 }
